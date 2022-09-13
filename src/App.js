d5e10d9b965eb5ad1e0d3ab126a89aaed8b79ae0
prit67 <20it108@charusat.edu.in>
2022-09-13 13:03:29 +0530

    Added Students Listing.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Student from './components/Student';
 import StudentList from './components/StudentList';
 import NavigationBar from './components/NavigationBar';
 import Footer from './components/Footer';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
     <div className="App">
+
       <NavigationBar/>
+      <Container>
       <Student/>
       <StudentList/>
+      </Container>
       <Footer/>
     </div>
   );
 }
-
 export default App;
